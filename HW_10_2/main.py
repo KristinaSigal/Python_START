0cd08e33afbe60dc9a4fe2dad6f4d5cd6baf9db7
kristina.sigal <kulrikh@gmail.com>
2023-01-12 19:53:01 +0300

    Add HW_10 task 2

---
+++ HW_10_2/main.py
@@ -0,0 +1,46 @@
+import telebot
+import HW4_4
+
+TOKEN = ""
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+Number = 0
+Greetings = "Привет! \n"
+
+Step = 0
+First = ''
+Second = ''
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, f'{Greetings} ')
+    bot.register_next_step_handler(message, Get_Polinom)
+
+# @bot.message_handler(func=lambda m: True)
+# def echo_all(message):
+
+
+def Get_Polinom(message):
+    global First
+    global Second
+    global Step
+    if Step == 0:
+        Step = 1
+        bot.send_message(message.from_user.id,
+                         f'Введите первый многочлен ')
+        bot.register_next_step_handler(message, Get_Polinom)
+    elif Step == 1:
+        Step = 2
+        First = message.text
+        bot.send_message(message.from_user.id,
+                         f'Введите второй многочлен ')
+        bot.register_next_step_handler(message, Get_Polinom)
+    elif Step == 2:
+        Step = 0
+        First = message.text
+        bot.send_message(
+            message.from_user.id, f'{HW4_4.sumPolinom(HW4_4.parsPolinom(First),HW4_4.parsPolinom(Second))}')
+
+
+bot.polling()
